2ca7b5c7a1a3c5ac2795253f7779afb5d8f5a052
Alex Lerner <alernerdev@gmail.com>
2016-11-29 16:53:00 -0500

    added mongodb connection to the code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var express = require('express');
+var mongoose = require('mongoose');
+var chalk = require('chalk');
 
 var app = express();
 app.set('views', __dirname);
@@ -10,6 +12,12 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
     res.render('index');
+});
+
+mongoose.connect('mongodb://localhost/jobfinder');
+var con = mongoose.connection;
+con.once('open', function() {
+    console.log(chalk.green('connected to mongodb successfully'));
 });
 
 app.listen(process.env.PORT || 3000);
